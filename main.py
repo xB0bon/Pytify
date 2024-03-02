19028bacf85f7790b1d60d4f1af2102955eef4ec
xB0bon <pan.bobon@gmail.com>
2024-03-02 18:10:48 +0100

    1.0.0

---
+++ main.py
@@ -0,0 +1,171 @@
+import os
+from tkinter import *
+from tkinter import filedialog
+from pygame import mixer
+import pygame
+
+nazwa_pliku = ''
+numer = None
+music_play = True
+count = 0
+pygame.mixer.init()
+
+
+def powrot():
+    global music_play
+    aktualny = int(listbox.curselection()[0])
+    listbox.selection_clear(0, END)  # Wyczyść zaznaczenie wszystkich linii
+    if listbox.size() > aktualny - 1 >= 0:
+        listbox.selection_set(aktualny - 1)  # Zaznacz następną linię
+    else:
+        listbox.selection_set(0)  # Jeśli przekroczyliśmy koniec listy, zaznacz pierwszą linijkę
+
+    music_play = True
+    play()
+
+
+def nastepna():
+    global music_play
+    aktualny = int(listbox.curselection()[0])
+    listbox.selection_clear(0, END)  # Wyczyść zaznaczenie wszystkich linii
+    if aktualny + 1 < listbox.size():
+        listbox.selection_set(aktualny + 1)  # Zaznacz następną linię
+    else:
+        listbox.selection_set(0)  # Jeśli przekroczyliśmy koniec listy, zaznacz pierwszą linijkę
+
+    music_play = True
+    play()
+
+
+def openfolder():
+    global folder
+    folder = filedialog.askdirectory()
+    print(folder)
+    if folder:
+        listbox.delete(0, END)
+        list_files = os.listdir(folder)
+        for file in list_files:
+            if file[-3:] == 'mp3':
+                listbox.insert(END, file)
+            else:
+                pass
+
+
+def play():
+    global music_play
+    global nazwa_pliku
+    global numer
+    if not music_play:
+        music_play = True
+        play_button.config(image=start)
+        mixer.music.pause()
+    elif music_play:
+        try:
+            music_play = False
+            play_button.config(image=stop)
+
+            if listbox.curselection()[0] == numer:
+                mixer.music.unpause()
+                update_progress()
+            else:
+                numer = listbox.curselection()[0]
+                nazwa_pliku = listbox.get(listbox.curselection()[0])
+                piosenka = pygame.mixer.Sound(f'{folder}/{nazwa_pliku}')
+                mixer.music.load(f'{folder}/{nazwa_pliku}')
+                mixer.music.play()
+                print(listbox.curselection()[0])
+                dlugosc = round(piosenka.get_length(), 1)
+                dlugosc1.set(str(dlugosc))
+                czas_muzyki.config(to=dlugosc)
+                nazwa_utworu.set(f'Utwór: {nazwa_pliku}')
+                update_progress()
+        except:
+            play_button.config(image=start)
+            pass
+
+
+def play_1():
+    global music_play, count
+    if music_play:
+        music_play = False
+    elif not music_play:
+        music_play = True
+    play()
+
+
+def update_progress():
+    global music_play
+    volume = glosnosc.get() / 10
+    mixer.music.set_volume(volume)
+    if not music_play:
+        current_time = pygame.mixer.music.get_pos() / 1000
+        czas.set(round(current_time, 1))
+        if str(round(current_time, 1) + 0.1) == dlugosc1.get():
+            print(listbox.size())
+            if listbox.curselection():
+                aktualny = int(listbox.curselection()[0])
+                listbox.selection_clear(0, END)  # Wyczyść zaznaczenie wszystkich linii
+                if aktualny + 1 < listbox.size():
+                    listbox.selection_set(aktualny + 1)  # Zaznacz następną linię
+                else:
+                    listbox.selection_set(0)  # Jeśli przekroczyliśmy koniec listy, zaznacz pierwszą linijkę
+
+                music_play = True
+                play()
+
+
+        window.after(100, update_progress)
+
+
+window = Tk()
+window.geometry("600x500")
+window.config(bg='black')
+frame = Frame(window)
+frame.config(bg='#2a2a2a')
+frame.pack()
+
+Label(frame, text='Pytify', font=('Arial', 20), fg='white', bg='#2a2a2a').pack()
+listbox = Listbox(frame, width=100, height=15, bg='#434143', fg='white', highlightbackground="black", bd=0)
+listbox.pack()
+open_button = Button(frame, text='Open', bg='#1fdf64', width=5, height=1, font=('Arial', 12, 'bold'),
+                     command=openfolder)
+open_button.place(x=525, y=3)
+nazwa_utworu = StringVar()
+Label(window, textvariable=nazwa_utworu, fg='white', bg='Black').pack()
+poprzedni = PhotoImage(file='next.png')
+nastepny = PhotoImage(file='previous.png')
+start = PhotoImage(file='play.png')
+stop = PhotoImage(file='pause.png')
+button_frame = Frame(window)
+button_frame.pack()
+
+Button(button_frame, command=powrot, image=poprzedni, bg='black', bd=0, highlightbackground="black",
+       activebackground="#1d1d1d").grid(
+    row=0, column=0)
+play_button = Button(button_frame, image=start, bg='black', bd=0, highlightbackground="black",
+                     activebackground="#1d1d1d", command=play)
+play_button.grid(row=0, column=1)
+Button(button_frame, command=nastepna, image=nastepny, bg='black', bd=0, highlightbackground="black",
+       activebackground="#1d1d1d").grid(
+    row=0, column=2)
+
+czas = DoubleVar(window, value=0)
+czas_muzyki = Scale(window, from_=0.0, to=0.01, resolution=0.01, orient=HORIZONTAL, variable=czas, length=500,
+                    bg='black', troughcolor='gray', sliderlength=20, highlightthickness=0, fg='white')
+czas_muzyki.pack(pady=10)
+dlugosc1 = StringVar(window)
+czas_max = Label(window, textvariable=dlugosc1, bg='black', fg='white')
+czas_max.pack(anchor=E)
+
+glosnosc_frame = Frame(window, bg='black')
+glosnosc_frame.pack(anchor=W)
+sound = PhotoImage(file='sound.png')
+Label(glosnosc_frame, image=sound, bg='black').grid(column=0, row=0)
+deafult = DoubleVar(window, value=1)
+glosnosc = Scale(glosnosc_frame, showvalue=False, from_=0.0, to=10.0, resolution=0.01, orient=HORIZONTAL,
+                 variable=deafult, length=150, bg='black', troughcolor='green', sliderlength=10, highlightthickness=0,
+                 fg='white')
+glosnosc.grid(column=1, row=0)
+listbox.bind("<<ListboxSelect>>", lambda event: play_1())
+window.bind("<space>", lambda event: play())
+window.mainloop()
